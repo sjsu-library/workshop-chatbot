83c5bc9de94637015f68fc4a2a9ca8027d8ff35a
Nick Szydlowski <nick.szydlowski@sjsu.edu>
2024-02-23 21:39:04 -0800

    troubleshooting

--- chat_with_documents.py
+++ chat_with_documents.py
@@ -86,7 +86,7 @@ llm = ChatOpenAI(
 )
 
 qa_chain = ConversationalRetrievalChain.from_llm(
-    llm, retriever=retriever, memory=memory, verbose=True, kwargs={"prompt":prompt}
+    llm, retriever=retriever, memory=memory, verbose=True
 )
 avatars = {"human": "user", "ai": "assistant"}
 for msg in msgs.messages:
