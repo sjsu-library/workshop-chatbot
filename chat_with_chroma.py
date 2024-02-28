8db47a930e726598d125a2e909e23ae89ffaa5ad
Nick Szydlowski <nick.szydlowski@sjsu.edu>
2024-02-28 10:52:58 -0800

    added ChromaDB connection as separate version

---
+++ chat_with_chroma.py
@@ -0,0 +1,87 @@
+import os
+import tempfile
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.document_loaders import JSONLoader
+from langchain_community.document_loaders.merge import MergedDataLoader
+from langchain.memory import ConversationBufferMemory
+from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.callbacks.base import BaseCallbackHandler
+from langchain.chains import ConversationalRetrievalChain
+from langchain.vectorstores import DocArrayInMemorySearch
+from langchain_community.vectorstores import Chroma
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.prompts import PromptTemplate
+from langchain.embeddings import OpenAIEmbeddings
+import chromadb
+
+st.set_page_config(page_title="Kingbot - SJSU Library", page_icon="🤖")
+st.title("Chat with SJSU Library's Kingbot 🤖")
+st.text("This experimental version of Kingbot uses Streamlit, LangChain, and ChatGPT.")
+@st.cache_resource(ttl="1h")
+def configure_retriever():
+    embedding = OpenAIEmbeddings(openai_api_key=openai_api_key)
+    client = chromadb.HttpClient(host='mlk-tutorial01.sjsu.edu', port=8000)
+    dbremote = Chroma(
+        client=client,
+        collection_name="sjsulib",
+        embedding_function=embedding,
+    )
+    retriever = dbremote.as_retriever()
+    return retriever
+class StreamHandler(BaseCallbackHandler):
+    def __init__(self, container: st.delta_generator.DeltaGenerator, initial_text: str = ""):
+        self.container = container
+        self.text = initial_text
+        self.run_id_ignore_token = None
+    def on_llm_start(self, serialized: dict, prompts: list, **kwargs):
+        # Workaround to prevent showing the rephrased question as output
+        if prompts[0].startswith("Human"):
+            self.run_id_ignore_token = kwargs.get("run_id")
+    def on_llm_new_token(self, token: str, **kwargs) -> None:
+        if self.run_id_ignore_token == kwargs.get("run_id", False):
+            return
+        self.text += token
+        self.container.markdown(self.text)
+class PrintRetrievalHandler(BaseCallbackHandler):
+    def __init__(self, container):
+        self.status = container.status("**Context Retrieval**")
+    def on_retriever_start(self, serialized: dict, query: str, **kwargs):
+        self.status.write(f"**Question:** {query}")
+        self.status.update(label=f"**Context Retrieval:** {query}")
+    def on_retriever_end(self, documents, **kwargs):
+        for idx, doc in enumerate(documents):
+            source = os.path.basename(doc.metadata["source"])
+            self.status.write(f"**Document {idx} from {source}**")
+            self.status.markdown(doc.page_content)
+        self.status.update(state="complete")
+
+
+openai_api_key = st.secrets["key"]
+
+retriever = configure_retriever()
+# Setup memory for contextual conversation
+msgs = StreamlitChatMessageHistory()
+memory = ConversationBufferMemory(memory_key="chat_history", chat_memory=msgs, return_messages=True)
+
+
+
+# Setup LLM and QA chain
+llm = ChatOpenAI(
+    model_name="gpt-3.5-turbo", openai_api_key=openai_api_key, temperature=0, streaming=True
+)
+
+qa_chain = ConversationalRetrievalChain.from_llm(
+    llm, retriever=retriever, memory=memory, verbose=True
+)
+avatars = {"human": "user", "ai": "assistant"}
+for msg in msgs.messages:
+    st.chat_message(avatars[msg.type]).write(msg.content)
+if user_query := st.chat_input(placeholder="Ask me anything!"):
+    st.chat_message("user").write(user_query)
+    with st.chat_message("assistant"):
+        retrieval_handler = PrintRetrievalHandler(st.container())
+        stream_handler = StreamHandler(st.empty())
+        response = qa_chain.run(user_query, callbacks=[retrieval_handler, stream_handler])
